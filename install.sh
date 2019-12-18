15f0e0e9d8cef9556f0025c1fc9805924e0adada
Proudust <proudust@gmail.com>
2019-12-18 13:57:18 +0900

    :bug: インストールが途中で止まるのを修正

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 
+git clone https://github.com/proudust/dotfiles.git ~/dotfiles
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
 sudo apt install -y build-essential curl file gcc git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+source ~/.bash_profile
 
 brew install golang npm python3 python@2
 brew install ghq jq peco unzip zip
-
-git clone https://github.com/proudust/dotfiles.git ~/dotfiles
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
