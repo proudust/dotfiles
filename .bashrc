691e6057c3efabdf3381d801900387de7b6476f1
Proudust <proudust@gmail.com>
2019-12-23 17:19:35 +0900

    :bug: git の設定を bashrc から gitconfig へ移動

--- .bashrc
+++ .bashrc
@@ -6,10 +6,6 @@ alias ..="cd .."
 alias python="python3"
 alias pip="pip3"
 
-git config --global ghq.root "$GOPATH/src"
-git config --global user.name "Proudust"
-git config --global user.email "proudust@gmail.com"
-
 function code(){
   if [ $1 ]; then
     local path=$(wslpath -w $1)
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt autoremove -y
 
 git clone https://github.com/proudust/dotfiles.git ~/dotfiles
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 sudo apt install -y build-essential curl file gcc git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
