fadef7f8c7f9870e85d96f8e504bbfd96b0ab6e5
Proudust <proudust@gmail.com>
2019-12-29 21:58:01 +0900

    :sparkles: Chocolatey と Git をインストールを追加

--- install.bat
+++ install.bat
@@ -3,6 +3,11 @@
 @REM Check admin
 OPENFILES > NUL 2>&1
 IF NOT %ERRORLEVEL% EQU 0 GOTO ERROR_NOT_ADMIN
+
+@REM Setup chocolatey and git
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+cinst git
+
 @REM Make symbolic link
 DEL "%HOMEPATH%\.gitconfig"
 SET DOTFILES_REPO=%HOMEPATH%\dotfiles
@@ -12,3 +17,11 @@ IF NOT EXIST %DOTFILES_REPO%\.git (
   git -C %DOTFILES_REPO% fetch origin & git -C %DOTFILES_REPO% pull origin
 )
 MKLINK "%HOMEPATH%\.gitconfig" "%DOTFILES_REPO%\.gitconfig"
+
+GOTO END
+
+:ERROR_NOT_ADMIN
+echo ERROR: Not admin user.
+GOTO END
+
+:END
