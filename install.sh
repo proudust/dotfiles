ca63d963d8aad1bb1d715a2c55ea02c1e464ce32
Proudust <proudust@gmail.com>
2020-04-02 13:24:39 +0900

    :rotating_light: ShellCheck の指摘を修正

--- install.sh
+++ install.sh
@@ -113,7 +113,7 @@ brew install python3 python@2
 echo '- Rust (rustup)'
 brew install rustup
 rustup-init
-$HOME/.cargo/bin/rustup target add x86_64-unknown-linux-musl
+"$HOME/.cargo/bin/rustup" target add x86_64-unknown-linux-musl
 if [ "$OS" = 'Mac' ]; then
   echo "- GUI"
   brew cask install font-cica google-backup-and-sync google-chrome google-japanese-ime visual-studio-code
