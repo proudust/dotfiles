c47310f5c1fe553de451ea3bec9ee4752a32d5c7
Proudust <proudust@gmail.com>
2021-03-22 00:39:14 +0900

    :fire: Chocolatey を使用しないように変更

--- install.bat
+++ install.bat
@@ -4,10 +4,6 @@
 OPENFILES > NUL 2>&1
 IF NOT %ERRORLEVEL% EQU 0 GOTO ERROR_NOT_ADMIN
 
-@ECHO Install chocolatey and git
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-cinst -ry --no-progress git
-
 @ECHO Clone dotfiles repo
 SET DOTFILES_REPO=%HOMEDRIVE%%HOMEPATH%\dotfiles
 IF NOT EXIST %DOTFILES_REPO%\.git (
@@ -32,16 +28,6 @@ dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /nores
 
 @ECHO Set registry
 REG IMPORT %DOTFILES_REPO%\registry.reg
-
-@ECHO Install packages
-cinst -ry --no-progress %DOTFILES_REPO%\Packages.config
-choco pin add -n=googlechrome
-choco pin add -n=vivaldi
-choco pin add -n=vscode
-choco pin add -n=discord
-choco pin add -n=minecraft
-choco pin add -n=steam
-choco pin add -n=origin
 
 @ECHO Uninstall unnecessary apps
 powershell "Get-AppxPackage king.com.CandyCrushSodaSaga | Remove-AppxPackage"
