635d8f3af2d5c4932a982cba7618f1f61e84ad27
Proudust <proudust@gmail.com>
2021-03-23 23:40:41 +0900

    :sparkles: winget によるソフトウェアインストールスクリプトを追加

---
+++ windows/packages.bat
@@ -0,0 +1,20 @@
+winget install -e 7zip.7zip
+winget install -e Docker.DockerDesktop
+winget install -e Git.Git
+winget install -e Microsoft.PowerToys
+winget install -e Microsoft.VisualStudioCode-System-x64 --override "/verysilent /mergetasks=""addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"""
+winget install -e Microsoft.WindowsTerminal
+winget install -e VivaldiTechnologies.Vivaldi
+
+winget uninstall Microsoft.549981C3F5F10_8wekyb3d8bbwe
+winget uninstall Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe
+winget uninstall Microsoft.Office.OneNote_8wekyb3d8bbwe
+winget uninstall Microsoft.People_8wekyb3d8bbwe
+winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c
+winget uninstall Microsoft.Wallet_8wekyb3d8bbwe
+winget uninstall Microsoft.WindowsCamera_8wekyb3d8bbwe
+winget uninstall microsoft.windowscommunicationsapps_8wekyb3d8bbwe
+winget uninstall Microsoft.WindowsMaps_8wekyb3d8bbwe
+winget uninstall OneDriveSetup.exe
+winget uninstall SpotifyAB.SpotifyMusic_zpdnekdrzrea0
